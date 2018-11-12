060877a19ddee210250574d2a777521ef0033a6a
hp <hp@LAPTOP-15N4ONKA>
2018-11-12 23:23:39 +0530

    Committed

---
+++ NativeTest/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,10 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void PostAppium() {
+		System.out.println("PostAppium");
+	}
+}
---
+++ NativeTest/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,25 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void Selenium() {
+		System.out.println("Selenium");
+	}
+	
+	@Test
+	public void Python() {
+		System.out.println("Python");
+	}
+	
+	@Test
+	public void Perl() {
+		System.out.println("Perl");
+	}
+}
